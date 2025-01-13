844e74f584a8b6dfd8b2b12632b2ee55ea1f25ea
Ariel-Git <arielmgarfield@gmail.com>
2025-01-13 14:45:51 +0200

    first commit

---
+++ client/src/components/screens/features/quiz/questionsScreen/QuizQuestionsScreen.vue
@@ -0,0 +1,25 @@
+<template>
+  <BackgroundWave />
+  <Navigator
+      :items="[
+        { text: name, to: `/feature/quiz` },
+        { text: 'Quiz', to: '' }
+      ]"
+    />
+  QUESTIONSss
+</template>
+
+<script>
+import BackgroundWave from '@/components/base/backgrounds/wave/BackgroundWave.vue'
+import Navigator from '@/components/compositions/navigator/Navigator.vue'
+
+export default {
+  name: 'QuizQuestionsScreen',
+  components: { BackgroundWave, Navigator },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
